12dbc471fb9e40f6c4e4a2cc1c078a8faea6a9a6
Emma Dickson <ejd315@nyu.edu>
2018-02-15 13:13:40 -0500

    form addition

--- js/init.js
+++ js/init.js
@@ -3,6 +3,6 @@
     $("#hidden").fadeIn("slow")
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
